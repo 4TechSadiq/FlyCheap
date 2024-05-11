4fc86d910fc9728831d0f7ddb55f4a432f604c5a
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-05-11 21:52:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ city_list = datamanager.get_city()
 price_list = DataManager().get_price()
 iata_code = DataManager().get_iata()
 
+datamanager.start()
 
 flyfrom= input("enter iata code : ")
 flyprice = int(input("enter max price : "))
@@ -16,6 +17,6 @@ for index in range(0, len(city_list)):
     flight = FlightSearch(flyfrom=flyfrom, flyto=iata_code[index], price=flyprice)
     flight_datah = flight.search_flight()
 
-    message += f"{index}- ${flight_datah[0].get('price')} to fly from {flight_datah[0].get('flyfrom')} to {flight_datah[0].get('flyto')} from {flight_datah[0].get('dateFrom')} to {flight_datah[0].get('dateTo')}.\n"
+    message += f"{index}) ${flight_datah[0].get('price')} to fly from {flight_datah[0].get('flyfrom')} to {flight_datah[0].get('flyto')} from {flight_datah[0].get('dateFrom')} to {flight_datah[0].get('dateTo')}.\n"
 
-NotificationManager().send_message(msg=message)
+NotificationManager().send_mail(message=message)
