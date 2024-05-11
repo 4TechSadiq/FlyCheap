508b029b2d7e75772cce9a9821a1ae1cc7615733
C.P. Mohammed Sadiq Ali <22ad064@excelcolleges.com>
2024-05-11 22:31:17 +0530

    Update notification_manager.py
    Added mail service

--- core/notification_manager.py
+++ core/notification_manager.py
@@ -1,16 +1,40 @@
 from twilio.rest import Client
+import smtplib
+import requests
 
+SHEETY2_GET_API = "USER SHEETY API"
+SHEETY_KEY = "SHEETY_KEY"
 
 class NotificationManager:
     def __init__(self):
         self.message = None
-        self.ssid = "[YOUR SSID]"
-        self.auth_token = "[AUTH_TOKEN]"
+        self.ssid = "SSID"
+        self.auth_token = "AUTH TOKEN"
         self.client = Client(self.ssid, self.auth_token)
+        self.mail = "EMAIL"
+        self.password = "APP PASSWORD"
+        self.header = {
+            "Authorization": SHEETY_KEY
+        }
+        self.response = requests.get(url=SHEETY2_GET_API, headers=self.header)
 
     def send_message(self,msg):
-        self.message = msg #Message to be sent
-        message = self.client.messages.create(body=self.message, from_="[TWILIO NUMBER]", to='[RECIEVER NUMBER]')
-
+        self.message = msg
+        message = self.client.messages.create(body=self.message, from_="+13344686812", to='+918714094884')
         print(message.sid)
+
+    def send_mail(self, message):
+        mail_data = self.response.json()
+        mail_list = []
+        for i in range(len(mail_data['users'])):
+            mail = mail_data['users'][i]['email']
+            mail_list.append(mail)
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user=self.mail, password=self.password)
+            for i in range(len(mail_list)):
+                connection.sendmail(from_addr=self.mail,
+                                    to_addrs= mail_list[i],
+                                    msg=f"Subject:Flycheap!!\n\n{message}"
+                                    )
 
